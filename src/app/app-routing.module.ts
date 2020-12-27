10242f1a0dda8d5961c9f79812a84b8262e5c37d
Chandu <chandu_boss@hotmail.com>
2020-12-27 18:06:22 +0530

    Added Home Screen with toggle screen. Initialized multiple components like: * Add Employee * Dashboard * Home * Login * SignUp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignUpComponent},
+  {path:'addEmployee',component:AddEmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatListModule} from '@angular/material/list';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { DashboardComponent } from './dashboard/dashboard.component'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignUpComponent,
+    AddEmployeeComponent,
+    DashboardComponent
   ],
   imports: [
+    MatListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
